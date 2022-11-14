186d5338387d0af74567fd7b2154f2ba3c704121
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2022-11-14 20:36:02 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -34,38 +34,72 @@ class Solution
     public:
     //Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node *head) {
-        
-        int ct1=0,ct2=0,ct3=0;
         Node *t=head;
-        while(t){
+        Node *one=NULL,*onet=NULL;
+        Node *zero=NULL,*zerot=NULL;
+        Node *two=NULL,*twot=NULL;
+        while(t!=NULL){
             if(t->data==0){
-                ct1++;
+                if(zero==NULL){
+                    zero=t;
+                    zerot=t;
+                }
+                else{
+                    zerot->next=t;
+                    zerot=zerot->next;
+                }
             }
-            if(t->data==1){
-                ct2++;
+            else if(t->data==1){
+                if(one==NULL){
+                    one=t;
+                    onet=t;
+                }
+                else{
+                    onet->next=t;
+                    onet=onet->next;
+                }
             }
-            if(t->data==2){
-                ct3++;
+            else if(t->data==2){
+                if(two==NULL){
+                    two=t;
+                    twot=t;
+                }
+                else{
+                    twot->next=t;
+                    twot=twot->next;
+                }
             }
             t=t->next;
         }
-        t=head;
-        while(t){
-            if(ct1){
-                t->data=0;
-                ct1--;
-            }
-            else if(ct2){
-                t->data=1;
-                ct2--;
-            }
-            else if(ct3){
-                t->data=2;
-                ct3--;
-            }
-            t=t->next;
+        
+        Node *ans=NULL;
+        Node *anst=NULL;
+        if(zero){
+            ans=zero;
+            anst=zerot;
         }
-        return head;
+        if(one){
+            if(anst){
+                anst->next=one;
+                anst=onet;
+            }
+            else{
+                ans=one;
+                anst=onet;
+            }
+        }
+        if(two){
+            if(anst){
+                anst->next=two;
+                anst=twot;
+            }
+            else{
+                ans=two;
+                anst=twot;
+            }
+        }
+        anst->next=NULL;
+        return ans;
     }
 };
 
