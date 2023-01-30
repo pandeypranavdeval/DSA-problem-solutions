2df655b9912643400cbdd7b571e0722dea8b3553
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-01-31 00:11:23 +0530

    Added solution - LeetHub

---
+++ Fruit Into Baskets - GFG/fruit-into-baskets.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalFruits(int n, vector<int> &fruits){
+        // ye ques pick toys
+        int i=0,j=0,ans=0;
+        unordered_map<int,int>m;
+        while(j<n){
+            m[fruits[j]]++;
+            if(m.size()<=2){
+                ans=max(ans,j-i+1);
+            }
+            if(m.size()>2){
+                while(m.size()>2){
+                    m[fruits[i]]--;
+                    if(m[fruits[i]]==0){
+                        m.erase(fruits[i]);
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> fruits(N);
+        for (int i = 0; i < N; i++) cin >> fruits[i];
+        Solution obj;
+        cout << obj.totalFruits(N, fruits) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
