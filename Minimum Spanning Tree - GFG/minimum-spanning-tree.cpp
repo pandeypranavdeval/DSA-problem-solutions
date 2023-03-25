0244c114a01964bef8f672cf8c1f65b1ccd9fd66
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-03-25 18:30:37 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,34 +3,71 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]){
-        priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int>vis(V,0);
-        pq.push({0,0});
-        int sum=0;
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int node=it.second;
-            int wt=it.first;
-            if(vis[node]){
-                continue;
-            }
-            vis[node]=1;
-            sum+=wt;
-            for( auto itr:adj[node]){
-                int adjNode=itr[0];
-                int edW=itr[1];
-                if(!vis[adjNode]){
-                    pq.push({edW,adjNode});
-                }
+
+class Disjointset{
+    vector<int>rank,parent,size;
+    public:
+        Disjointset(int n){
+            rank.resize(n+1,0);
+            parent.resize(n+1);
+            size.resize(n+1);
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
+                size[i]=1;
             }
         }
-        return sum;
+        int findupar(int node){
+            if(node==parent[node]){
+                return node;
+                
+            }
+            return parent[node]=findupar(parent[node]);
+        }
+        void findunionbySize(int u,int v){
+            int upu=findupar(u);
+            int upv=findupar(v);
+            if(upu==upv){
+                return ;
+            }
+            if(size[upu]<size[upv]){
+                parent[upu]=upv;
+                size[upv]+=size[upu];
+            }
+            else{
+                parent[upv]=upu;
+                size[upu]+=size[upv];
+            }
+        }
+};
+
+class Solution
+{   
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<pair<int,pair<int,int>>>edges;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int adjNode=it[0];
+                int wt=it[1];
+                int node=i;
+                edges.push_back({wt,{node,adjNode}});
+            }
+        }
+        Disjointset ds(V);
+        sort(edges.begin(),edges.end());
+        int mstwt=0;
+        for(auto it:edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            if(ds.findupar(u)!=ds.findupar(v)){
+                mstwt+=wt;
+                ds.findunionbySize(u,v);
+            }
+        }
+        return mstwt;
     }
 };
 
