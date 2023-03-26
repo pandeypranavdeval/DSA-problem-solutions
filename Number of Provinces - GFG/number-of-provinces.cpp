b80cb37d44b0418f497245adb2436bd93d725724
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-03-26 11:14:17 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -6,37 +6,64 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution {
-  public:
-  void dfs(int node,vector<int>adjls[],vector<int>&vis ){
-        vis[node]=1;
-        for(auto &it:adjls[node]){
-            if(!vis[it]){
-                dfs(it,adjls,vis);
+
+class Disjointset{
+    // class of disjoint set ds can be used everywhere ds is needed
+    
+    public:
+        vector<int>rank,parent,size;
+        Disjointset(int n){
+            rank.resize(n+1,0);
+            parent.resize(n+1);
+            size.resize(n+1);
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
+                size[i]=1;
             }
         }
-    }
-    
+        int findupar(int node){
+            if(node==parent[node]){
+                return node;
+                
+            }
+            return parent[node]=findupar(parent[node]);
+        }
+        void findunionbySize(int u,int v){
+            int upu=findupar(u);
+            int upv=findupar(v);
+            if(upu==upv){
+                return ;
+            }
+            if(size[upu]<size[upv]){
+                parent[upu]=upv;
+                size[upv]+=size[upu];
+            }
+            else{
+                parent[upv]=upu;
+                size[upu]+=size[upv];
+            }
+        }
+};
+
+class Solution {
+  public:
+  
     int numProvinces(vector<vector<int>> adj, int V) {
-        // code here
-         int ans=0;
-        vector<int>adjls[V];
+        Disjointset ds(V);
         for(int i=0;i<V;i++){
             for(int j=0;j<V;j++){
-                if(adj[i][j]&&i!=j){
-                    adjls[i].push_back(j);
-                    //adjls[j].push_back(i);
+                if(adj[i][j]==1){
+                    ds.findunionbySize(i,j);
                 }
             }
         }
-        vector<int>vis(V,0);
+        int ct=0;
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                ans++;
-                dfs(i,adjls,vis);
+            if(ds.parent[i]==i){
+                ct++;
             }
         }
-        return ans;
+        return ct;
     }
 };
 
