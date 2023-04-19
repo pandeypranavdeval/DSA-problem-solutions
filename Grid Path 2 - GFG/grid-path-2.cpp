65405480f1392b699213969ba4f7b9636056d2c8
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-04-19 16:13:57 +0530

    Added solution - LeetHub

--- Grid Path 2 - GFG/grid-path-2.cpp
+++ Grid Path 2 - GFG/grid-path-2.cpp
@@ -5,33 +5,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int M =1e9+7;
-  int fun(int i,int j,vector<vector<int>>&dp,vector<vector<int>>& oG){
-        if(i<0||j<0){
-            return 0;
-        }
-        if(oG[i][j]==1){
-            return 0;
-        }
-        if(i==0&&j==0){
-            return 1;
-        }
-        
-        if(oG[i][j]==1){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int w1=fun(i-1,j,dp,oG);
-        int w2=fun(i,j-1,dp,oG);
-        return dp[i][j]=(w1+w2)%M;
-    }
+    int M=(int)1e9+7;
     int totalWays(int n, int m, vector<vector<int>>& grid) {
         // Code here
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        int ans=fun(n-1,m-1,dp,grid);
-        return ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    dp[i][j]=0;
+                }
+                else if(i==0&&j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int u=0;
+                    int v=0; 
+                    if(i>0){
+                        u=dp[i-1][j];
+                    }
+                    if(j>0){
+                        v=dp[i][j-1];
+                    }
+                    dp[i][j]=(u+v)%M;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
 
