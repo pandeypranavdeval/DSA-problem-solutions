f2f6cd9b34d7ad4ba52858c4cfaf63ac61b7305f
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-04-20 12:02:17 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -5,23 +5,22 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int calminPath(int i,int j,vector<vector<int>>& tri,vector<vector<int>>&dp){
-        int n=tri.size();
-        if(i==n-1){
-            return tri[i][j];
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int down=tri[i][j]+calminPath(i+1,j,tri,dp);
-        int diag=tri[i][j]+calminPath(i+1,j+1,tri,dp);
-        return dp[i][j]=min(down,diag);
-    }
+    
     int minimizeSum(int n, vector<vector<int>>& triangle){
+        // bottom up tabulation soln
+        //int n=triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans=0;
-        ans=calminPath(0,0,triangle,dp);
-        return ans;
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle[i][j]+dp[i+1][j];
+                int diag=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diag);
+            }
+        }
+        return dp[0][0];
     }
 };
 
