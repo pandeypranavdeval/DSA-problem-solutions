e37a1fe3e3dd5cbde24f149c1e21e443190f4807
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-09-21 20:43:01 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -22,9 +22,13 @@ class Solution
         return dp[ind]=max(pick,notpick);
     }
     int FindMaxSum(int arr[], int n){
-        vector<int>dp(n,-1);
-        int ans=solve(n-1,arr,n,dp);
-        return ans;
+        vector<int>dp(n,0);
+        dp[0]=arr[0];
+        dp[1]=max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(arr[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
 
