a6637ced8c1ab8aec83e6eb11983d13d0a2761fb
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-09-26 01:18:20 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,26 +6,35 @@ using namespace std;
 class Solution{
 
   public:
-	int minDifference(int v[], int n)  { 
-	    long long sum=0;
+	int minDifference(int arr[], int n)  { 
+	    int sum=0;
 	    for(int i=0;i<n;i++){
-	        sum+=v[i];
+	        sum+=arr[i];
 	    }
-	    int dp[sum+1];
-	    memset(dp,0,sizeof(dp));
-	    dp[0]=1;
-	    for(int i=0;i<n;i++){
-	        for(long long j=sum;j>=v[i];j--){
-	            dp[j]|=dp[j-v[i]];
+	    vector<vector<bool>>dp(n+1,vector<bool>(sum+1));
+	    for(int i=0;i<n+1;i++){
+	        dp[i][0]=true;
+	    }
+	    for(int j=1;j<sum+1;j++){
+	        dp[0][j]=false;
+	    }
+	    for(int i=1;i<n+1;i++){
+	        for(int j=1;j<sum+1;j++){
+	            if(arr[i-1]<=j){
+	                dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+	            }
+	            else{
+	                dp[i][j]=dp[i-1][j];
+	            }
 	        }
 	    }
-	    long long s=sum/2+sum%2;
-	    while(dp[s]==0){
-	        s--;
+	    int ans=INT_MAX;
+	    for(int j=0;j<=sum/2;j++){
+	        if(dp[n][j]){
+	            ans=min(ans,sum-2*j);
+	        }
 	    }
-	    return abs((sum-s)-s);
-	    
-	    
+	    return ans;
 	} 
 };
 
