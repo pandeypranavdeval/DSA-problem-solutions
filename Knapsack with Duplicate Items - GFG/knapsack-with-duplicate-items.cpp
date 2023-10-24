c3a0ec091ee8e151282302facb067d8da80aaa0a
Pranav Deval Pandey <75794523+pandeypranavdeval@users.noreply.github.com>
2023-10-25 00:15:18 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,27 +9,26 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int ind,int w,int val[], int wt[],vector<vector<int>>&dp){
-        if(ind==0||w==0){
-            return 0;
+    int knapSack(int n, int w, int val[], int wt[]){
+        // tabulation dp
+        vector<vector<int>>dp(n+1,vector<int>(w+1));
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
         }
-        if(dp[ind][w]!=-1){
-            return dp[ind][w];
+        for(int j=0;j<w+1;j++){
+            dp[0][j]=0;
         }
-        if(wt[ind-1]<=w){
-            dp[ind][w]=max(val[ind-1]+solve(ind,w-wt[ind-1],val,wt,dp),
-            solve(ind-1,w,val,wt,dp));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<w+1;j++){
+                if(wt[i-1]<=j){
+                    dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        else{
-            dp[ind][w]=solve(ind-1,w,val,wt,dp);
-        }
-        return dp[ind][w];
-    }
-    int knapSack(int n, int w, int val[], int wt[])
-    {
-        vector<vector<int>>dp(n+1,vector<int>(w+1,-1));
-        int ans=solve(n,w,val,wt,dp);
-        return ans;
+        return dp[n][w];
     }
 };
 
